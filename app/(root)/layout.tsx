b66a66db47d69dd8cffe50b3999309377e3b6155
Keshav <chaoskizmo9416@gmail.com>
2025-07-14 12:44:36 +0530

    Change YOC.ai text and logo

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -17,7 +17,6 @@ const Rootlayout = async ({ children }: { children: ReactNode }) => {
       <nav>
           <Link href="/" className='flex items-center gap-2'>
               <Image src="/grid99.png" alt='Logo' width={38} height={32} />
-
               <h2 className='text-primary-100'>YOC.ai</h2>
           </Link>
       </nav> 
